f9c4d748fd32ef7961488671034d83b8d95c32bc
yesyue00 <68411761+yesyue00@users.noreply.github.com>
2022-08-28 23:55:59 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mkdir -p /root/.ssh
+mkdir .ssh
 cd .ssh
-touch authorized_keys
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCypMlvtPThGIt18Zmt17Fho70LlpwvSbuasAvJ0M0Z7yXYUS4jNFJne9ftpnKV2LKzLj33N1IJBH4C7zkGI8BXcWDFpC7Kc+k+72lT+5Ja+iAwXg/7q51zakljgYbseAah/AMm2VfYUhOOEzdYo8A1tS+1NZY92yrbbeVmu5QcypAwuNlCIDi6h47+6d+QbJppmzNSStPTgoI+W36hzFvnbOYiPpqIvnQbtaXKnpxMHAvWRypBu+HWfwkyxVzoKLDVTNwE+JaGsAEmgAcHmshW8p4hZFQtRoeXjl9NFv6Xy+CZ3hgMLdLIW3z00fm1HSrEtCwx6hPM5uU/cHMX18GC+zRWfkLTNmX/QFq1fl1RAkx0GiQTqh8RBpHTrz3LZJofmLz8PHCxsgYd6Tx4d/dyjuxS3CWGgTtFAYiQR8gUsVDpR/4nPcBYA8yiRbtPxxRyfV4iL3CTyrLjNjSrgdspRIpHV2DBv0GhZZmB0qw0w3ghXWZKBQorje411TY15P0= mas@DESKTOP-PVAQ8FO" >> authorized_keys
